ba375e0c6195213d06c98b18c5866b0993317fb8
Phil Nguyen <pcn@cs.umd.edu>
2014-05-12 23:01:02 -0400

    convert string to other datatypes

--- code/load.rkt
+++ code/load.rkt
@@ -12,32 +12,42 @@
 ;; (associative lists are usually faster that hash tables for <20 elements)
 
 ;; retrives 'label' attribute of all given XExpr with given tag
-(define (label-at xexprs tag)
+(define (project xexprs tag key)
   (for/list ([a (filter-tag tag xexprs)])
-    (lookup (second a) 'label)))
+    (lookup (second a) key)))
+
+;; convert string to other data
+(define (string->data s t)
+  ((match t
+     [(or "float" "int" "integer" "real" "number") string->number]
+     [(or "bool" "boolean") (match-lambda ["true" #t] ["false" #f])]
+     [_ identity])
+   s))
 
 ;; XExpr String? -> (Input-Port -> Dtb)
 (define ((mk-class-loader class-desc [delim "\t"]) fn)
   (match class-desc
     [(<> class (@: [name name]) body ...)
      (let ([prim-ids (string-split (rob/class->primary-id class-desc) ",")]
-           [attributes (label-at body 'attribute)]
-           [references (label-at body 'reference)]
-           [collections (label-at body 'collection)])
+           [attributes (project body 'attribute 'label)]
+           [types (project body 'attribute 'type)]
+           [references (project body 'reference 'label)]
+           [collections (project body 'collection 'label)])
        ;; first pass loading pre-objects
        (for/hash ([l (rest (file->lines fn))])
-         (let* ([fields (string-split l delim)]
+         (let* ([fields (for/list ([f (string-split l delim)] [t types])
+                          (string->data f t))]
                 [obj (map list attributes fields)]
                 [id (lookup/list obj prim-ids)])
            (values id obj))))]))
 
 (define x ((mk-class-loader `(class ([name exon_expr])
                      (primaryId ([field "barcode,exon"]))
-                     (attribute ([label "barcode"]))
-                     (attribute ([label "exon"]))
-                     (attribute ([label "raw_counts"]))
-                     (attribute ([label "median_length_normalized"]))
-                     (attribute ([label "RPKM"]))
-                     (reference ([label "exon"]))
-                     (reference ([label "barcode"]))))
+                     (attribute ([label "barcode"] [type "string"]))
+                     (attribute ([label "exon"] [type "string"]))
+                     (attribute ([label "raw_counts"] [type "float"]))
+                     (attribute ([label "median_length_normalized"] [type "float"]))
+                     (attribute ([label "RPKM"] [type "float"]))
+                     (reference ([label "exon"] [type "Exon"]))
+                     (reference ([label "barcode"] [type "Experiment"]))))
  "test.data"))
