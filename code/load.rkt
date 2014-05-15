b62f1ef7e37cd29ed49a2c762e6c918e03c7fc16
Phil Nguyen <pcn@cs.umd.edu>
2014-05-15 12:46:54 -0400

    slight clean up

--- code/load.rkt
+++ code/load.rkt
@@ -6,44 +6,11 @@
 
 (struct table-desc (name primary-ids attributes references collections) #:transparent)
 
-(define (dtb-name->idx dtb tab-name col-name)
-  (let ([tab (table-by-name dtb tab-name)])
-    (for/first ([attr (table-desc-attributes (first tab))]
-                [i (in-naturals)]
-                #:when (equal? col-name (second attr)))
-      i)))
-
-;; XExpr -> TableDesc
-(define (xexpr->table-desc xexpr)
-  ;; retrieves 'label' attribute of all given XExpr with given tag
-  (define (project xexprs tag key)
-    (for/list ([x (filter-tag tag xexprs)])
-      (lookup (xexpr->attributes x) key)))
-  
-  (match xexpr
-    [(<> class (@: [name name]) body ...)
-     (let ([prim-ids (string-split (rob/class->primary-id xexpr) ",")]
-           [attribute-labels (project body 'attribute 'label)]
-           [attribute-names (project body 'attribute 'name)]
-           [attribute-types (project body 'attribute 'type)]
-           [reference-labels (project body 'reference 'label)]
-           [reference-names (project body 'reference 'name)]
-           [reference-types (project body 'reference 'type)]
-           [collection-names (project body 'collection 'name)]
-           [collection-types (project body 'collection 'type)])
-       (table-desc name
-                   prim-ids
-                   (map list attribute-labels attribute-names attribute-types)
-                   (map list reference-labels reference-names reference-types)
-                   (map list collection-names collection-types)))]
-    [desc (error "BS class description" desc)]))
-
 ;; TableDesc (Id -> Int) -> (Table (Listof Val) -> Void)
 (define (make-updater desc label->idx)
   ;; TODO references and collecions
   (let* ([a-labels (map first (table-desc-attributes desc))]
          [a-names (map second (table-desc-attributes desc))]
-         [a-indices (map label->idx a-labels)]
          [a-converters (map (∘ type->converter third) (table-desc-attributes desc))]
          [id-names (table-desc-primary-ids desc)])
     (λ (table fields)
@@ -89,36 +56,6 @@
           (for ([(main-ids main-fields) (in-hash main-tab)])
             )))))
 
-;; (Listof (TableDesc Table)) String -> Table
-(define (table-by-name dtb name)
-  (for*/first ([tb dtb]
-               [tb-name (in-value (table-desc-name (first tb)))]
-               #:when (equal? name tb-name))
-    tb))
-
-;; convert string to other data
-(define (type->converter t)
-  (match t
-    [(or "float" "int" "integer" "real" "number") string->number]
-    #;[(or "bool" "boolean")
-     (λ (s)
-       (cond [(or (string-ci=? s "yes") (string-ci=? s "true")) #t]
-             [(or (string-ci=? s "no") (string-ci=? s "false")) #f]
-             [else (error "Don't know how to convert to boolean" s)]))]
-    [_ identity]))
-
-;; String -> (List String String)
-(define (string->path s) (string-split s "."))
-
-;; String -> (Any * -> Any)
-(define string->op ; TODO this assumes correct args
-  (match-lambda
-    ["=" equal?] ["<" <] [">" >] ["<=" <=] [">=" >=]
-    ["sum" +] ["prod" *]
-    ["mean" (λ xs (/ (apply + xs) (length xs)))]
-    ["max" max] ["min" min]
-    [x (error "Unknown operation" x)]))
-
 ;; Dtb Xexpr -> Any
 (define (query dtb xexpr)
   (match xexpr
@@ -146,3 +83,56 @@
               [else (error "Unknown path(s), check for typos" (list view path))]))]
          [else (error "Query enot supported yet" xexpr)]))]
     [q (error "Query not supported yet" q)]))
+
+;; XExpr -> TableDesc
+(define (xexpr->table-desc xexpr)
+  (match xexpr
+    [(<> class (@: [name name]) body ...)
+     (let ([prim-ids (string-split (rob/class->primary-id xexpr) ",")]
+           [attributes (for/list ([x (filter-tag 'attribute body)])
+                         (lookup/list (xexpr->attributes x) '(label name type)))]
+           [references (for/list ([x (filter-tag 'reference body)])
+                         (lookup/list (xexpr->attributes x) '(label name type)))]
+           [collections (for/list ([x (filter-tag 'collection body)])
+                          (lookup/list (xexpr->attributes x) '(name type)))])
+       (table-desc name prim-ids attributes references collections))]
+    [desc (error "BS class description" desc)]))
+
+;; Dtb String String -> Int
+;; return column index (counting from 0) for given column name in dtb name
+(define (dtb-name->idx dtb tab-name col-name)
+  (let ([tab (table-by-name dtb tab-name)])
+    (for/first ([attr (table-desc-attributes (first tab))]
+                [i (in-naturals)]
+                #:when (equal? col-name (second attr)))
+      i)))
+
+;; String -> (Any * -> Any)
+(define string->op ; TODO this assumes correct args
+  (match-lambda
+    ["=" equal?] ["<" <] [">" >] ["<=" <=] [">=" >=]
+    ["sum" +] ["prod" *]
+    ["mean" (λ xs (/ (apply + xs) (length xs)))]
+    ["max" max] ["min" min]
+    [x (error "Unknown operation" x)]))
+
+;; (Listof (TableDesc Table)) String -> Table
+(define (table-by-name dtb name)
+  (for*/first ([tb dtb]
+               [tb-name (in-value (table-desc-name (first tb)))]
+               #:when (equal? name tb-name))
+    tb))
+
+;; convert string to other data
+(define (type->converter t)
+  (match t
+    [(or "float" "int" "integer" "real" "number") string->number]
+    #;[(or "bool" "boolean")
+       (λ (s)
+         (cond [(or (string-ci=? s "yes") (string-ci=? s "true")) #t]
+               [(or (string-ci=? s "no") (string-ci=? s "false")) #f]
+               [else (error "Don't know how to convert to boolean" s)]))]
+    [_ identity]))
+
+;; String -> (List String String)
+(define (string->path s) (string-split s "."))
